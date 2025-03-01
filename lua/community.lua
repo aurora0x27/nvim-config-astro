a31e12e88b03590b573b05f2544b243a32767fe5
aurora0x27 <qingfengzl0831@163.com>
2025-03-01 09:58:29 +0800

    enable xmake.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -5,7 +5,7 @@
 ---@type LazySpec
 return {
     'AstroNvim/astrocommunity',
-    { import = 'astrocommunity.pack.lua' },
+    -- { import = 'astrocommunity.pack.lua' },
     { import = 'astrocommunity.colorscheme.catppuccin' },
     -- import/override with your plugins folder
 }
